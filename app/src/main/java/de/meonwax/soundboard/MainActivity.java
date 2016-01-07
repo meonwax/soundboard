0be50339e6bb84e354a97134dd7584e0039c712b
meonwax <meonwax@meonwax.de>
2016-01-07 14:44:49 +0100

    Introduced Toolbar as Actionbar

--- app/src/main/java/de/meonwax/soundboard/MainActivity.java
+++ app/src/main/java/de/meonwax/soundboard/MainActivity.java
@@ -6,6 +6,7 @@ import android.media.SoundPool;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import java.util.ArrayList;
@@ -20,6 +21,8 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         initSounds();
     }
 
